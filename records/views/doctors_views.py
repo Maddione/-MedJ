d5ca84c29e566c10a84d99e3e10a8dd09bf90114
444 <tgu000000@gmail.com>
2025-09-07 05:16:56 +0300

    perfect, DO NOT TOUCH AGAIN

--- records/views/doctors_views.py
+++ records/views/doctors_views.py
@@ -1,32 +1,26 @@
-from django.db import models
 from django.contrib.auth.decorators import login_required
 from django.http import JsonResponse
 from django.views.decorators.http import require_GET
+from django.db.models import Q, Case, When, IntegerField
 from records.models import Practitioner
 
 @login_required
 @require_GET
 def doctors_suggest(request):
     q = (request.GET.get("q") or "").strip()
-    spec_id = request.GET.get("specialty_id") or ""
+    specialty_id = request.GET.get("specialty_id")
     qs = Practitioner.objects.filter(owner=request.user, is_active=True)
     if q:
         qs = qs.filter(full_name__icontains=q)
-    if spec_id:
-        try:
-            sid = int(spec_id)
-            qs = qs.order_by(
-                models.Case(
-                    models.When(specialty_id=sid, then=models.Value(0)),
-                    default=models.Value(1),
-                    output_field=models.IntegerField(),
-                ),
-                "full_name",
+    if specialty_id:
+        qs = qs.annotate(
+            prio=Case(
+                When(specialty_id=specialty_id, then=0),
+                default=1,
+                output_field=IntegerField(),
             )
-        except Exception:
-            qs = qs.order_by("full_name")
+        ).order_by("prio", "full_name")
     else:
         qs = qs.order_by("full_name")
-    qs = qs.select_related("specialty")[:10]
-    results = [{"id": p.id, "full_name": p.full_name, "specialty_id": p.specialty_id or None} for p in qs]
-    return JsonResponse({"results": results})
+    items = [{"id": p.id, "full_name": p.full_name, "specialty_id": p.specialty_id} for p in qs[:10]]
+    return JsonResponse({"results": items})
